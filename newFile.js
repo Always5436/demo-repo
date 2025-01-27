9863259e9635e15e74dcacc463a4b677ca9211d2
utkarshpathak44 <utkarsh.pathak@remotestate.com>
2025-01-27 15:16:09 +0530

    Update newFile.js

--- newFile.js
+++ newFile.js
@@ -1,3 +1,9 @@
 console.log("this is a messgae")
 
 //create an event listener here to add elements dynamically
+
+const addButton=document.querySelector(".add")
+
+addButton.addEventListener("click",(event)=>{
+    //add the list elements here
+})
